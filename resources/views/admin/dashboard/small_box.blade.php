1cee825e926330852c076446df33512651528318
vikarmaulanaarrisyad <vikar.maulana.arrisyad@gmail.com>
2023-05-29 14:52:18 +0700

    create component , include databables yajra

---
+++ resources/views/admin/dashboard/small_box.blade.php
@@ -0,0 +1,57 @@
+  <div class="row">
+      <div class="col-lg-3 col-6">
+          <div class="small-box bg-info">
+              <div class="inner">
+                  <h3>150</h3>
+                  <p>Total Kendaraan</p>
+              </div>
+              <div class="icon">
+                  <i class="fas fa-motorcycle"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+      </div>
+
+      <div class="col-lg-3 col-6">
+
+          <div class="small-box bg-success">
+              <div class="inner">
+                  <h3>53</h3>
+                  <p>Kendaraan Masuk Mahasiswa</p>
+              </div>
+              <div class="icon">
+                  <i class="fas fa-motorcycle"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+      </div>
+
+      <div class="col-lg-3 col-6">
+
+          <div class="small-box bg-warning">
+              <div class="inner">
+                  <h3>44</h3>
+                  <p>Kendaraan Keluar Mahasiswa</p>
+              </div>
+              <div class="icon">
+                  <i class="fas fa-motorcycle"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+      </div>
+
+      <div class="col-lg-3 col-6">
+
+          <div class="small-box bg-danger">
+              <div class="inner">
+                  <h3>65</h3>
+                  <p>Kendaraan Keluar Umum</p>
+              </div>
+              <div class="icon">
+                  <i class="fas fa-motorcycle"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+      </div>
+
+  </div>
